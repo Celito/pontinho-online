044be90534bdbc5054129ca890c3b8e04af78fa6
Fernanda Bezerra <fbn.prg@gmail.com>
2020-05-03 11:00:35 -0300

    Fix: list route

--- pontinho-back-end/route/game-state-route.js
+++ pontinho-back-end/route/game-state-route.js
@@ -3,5 +3,5 @@ const controller = require("../controller/game-state-controller.js");
 
 module.exports = function(app){
     app.post("/api/match/create", controller.createGame);
-    app.post("/api/match", controller.listMatches);
+    app.get("/api/match", controller.listMatches);
 }
