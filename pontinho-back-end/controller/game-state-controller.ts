090740d0a85ffb7cf8114b7e42063be3580d66f2
Celito Felipetto <celito.felipetto@libertymutual.com>
2024-02-04 12:24:45 -0400

    using async functions instead of then statements;

--- pontinho-back-end/controller/game-state-controller.ts
+++ pontinho-back-end/controller/game-state-controller.ts
@@ -20,103 +20,63 @@ export class GameStateController implements IControllerBase {
     app.post("/api/match/join", (req, res) => this.joinMatch(req, res));
   }
 
-  createGame(req: Request, res: Response) {
-    PlayerModel.findOne({ name: req.body.name })
-      .then((player: IPlayer | null) => {
-        if (player) {
-          res.status(409).send("You are already in a match, not possible to be in two matches at the same time");
-        }
-        else {
-          const newPlayer = new PlayerModel(req.body);
-          newPlayer
-            .save()
-            .then((np: IPlayer) => {
-              const newGameState = new GameStateModel();
-              newGameState
-                .save()
-                .then((ngs: IGameState) => {
-                  const ngsPlayers = ngs.players;
-                  ngsPlayers.push(np._id);
-                  this.wsMatchGroups[ngs.id] = new Match(ngs.id);
-                  GameStateModel
-                    .findByIdAndUpdate(
-                      ngs._id,
-                      { $set: { players: ngsPlayers, host: np.name } },
-                      { new: true }
-                    )
-                    .populate('players')
-                    .then(updatedGameState => {
-                      if (updatedGameState) {
-                        res.status(200)
-                          .send(
-                            GameStateController.filterGameStateForPlayer(updatedGameState, np._id.toString())
-                          );
-                      }
-                    })
-                })
-            })
-            .catch((err => {
-              console.log(err);
-            }));
-        }
-      });
+  async createGame(req: Request, res: Response) {
+    const existingPlayer = await PlayerModel.findOne({ name: req.body.name })
+    if (existingPlayer) {
+      return res.status(409).send("You are already in a match, not possible to be in two matches at the same time");
+    }
+    const newPlayer = await (new PlayerModel(req.body)).save();
+    const newGameState = await (new GameStateModel()).save();
+    const ngsPlayers = newGameState.players;
+    ngsPlayers.push(newPlayer._id);
+    this.wsMatchGroups[newGameState.id] = new Match(newGameState.id);
+    const updatedGameState = await GameStateModel.findByIdAndUpdate(
+      newGameState._id,
+      { $set: { players: ngsPlayers, host: newPlayer.name } },
+      { new: true }
+    ).populate('players')
+    if (!updatedGameState) {
+      return res.status(500).send("Failed to generate the game state");
+    }
+    res.status(200).send(
+      GameStateController.filterGameStateForPlayer(updatedGameState, newPlayer._id.toString())
+    );
   }
 
-  listMatches(_: Request, res: Response) {
-    GameStateModel.find()
-      .then(gameState => {
-        if (gameState) {
-          const resJson = gameState.map((gs) => {
-            return { _id: gs._id, host: gs.host }
-          })
-          res.send(resJson);
-        }
-        else {
-          res.send([]);
-        }
-      });
+  async listMatches(_: Request, res: Response) {
+    const gameState = await GameStateModel.find() || []
+    const resJson = gameState.map((gs) => {
+      return { _id: gs._id, host: gs.host }
+    })
+    res.send(resJson);
   }
 
-  joinMatch(req: Request, res: Response) {
-    PlayerModel.findOne({ name: req.body.name })
-      .then(player => {
-        if (player) {
-          res.status(409).send("You are already in a match, not possible to be in two matches at the same time");
-        }
-        else {
-          const newPlayer = new PlayerModel(req.body);
-          newPlayer
-            .save()
-            .then(np => {
-              GameStateModel.findById({ _id: req.body.match_id })
-                .then(gameState => {
-                  if (!gameState) {
-                    return;
-                  }
-                  const newPlayersList = gameState.players;
-                  newPlayersList.push(np._id);
-                  GameStateModel
-                    .findByIdAndUpdate(
-                      gameState._id,
-                      { $set: { players: newPlayersList } },
-                      { new: true }
-                    )
-                    .populate('players')
-                    .then(updatedGameState => {
-                      if (!updatedGameState) {
-                        return;
-                      }
-                      res.send(
-                        GameStateController.filterGameStateForPlayer(updatedGameState, np._id.toString())
-                      );
-                    });
-                });
-            })
-            .catch((err => {
-              console.log(err);
-            }));
-        }
-      })
+  async joinMatch(req: Request, res: Response) {
+    const existingPlayer = await PlayerModel.findOne({ name: req.body.name })
+    if (existingPlayer) {
+      return res.status(409).send("You are already in a match, not possible to be in two matches at the same time");
+    }
+    const newPlayer = await (new PlayerModel(req.body)).save();
+    if (!newPlayer) {
+      return res.status(500).send("Failed to create a new player for your match");
+    }
+    const gameState = await GameStateModel.findById({ _id: req.body.match_id })
+    if (!gameState) {
+      return res.status(404).send("Failed to find the match you are trying to join");;
+    }
+    const newPlayersList = gameState.players;
+    newPlayersList.push(newPlayer._id);
+    const updatedGameState = await GameStateModel.findByIdAndUpdate(
+      gameState._id,
+      { $set: { players: newPlayersList } },
+      { new: true }
+    ).populate('players')
+    if (!updatedGameState) {
+      return res.status(500).send("Failed to join the match");
+    }
+    res.send(
+      GameStateController.filterGameStateForPlayer(updatedGameState, newPlayer._id.toString())
+    );
   }
 
   static getGameState(req: Request, res: Response): void {
